2ac69652d39fc03fb68d08e86695da626bfdba45
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2026-06-08 23:34:41 +0530

    primitive and non primitive

---
+++ JavaScript/03-datatypes/first.js
@@ -0,0 +1,21 @@
+//* Primitive Datatype
+let a = 10;
+let b = 20;
+console.log(a == b);
+
+let str1 = "Hello";
+let str2 = "Hello";
+console.log(str1 == str2);
+
+// *Non-Primitive Datatype
+let arr1 = [1, 2, 3];
+let arr2 = [1, 2, 3];
+console.log(arr1 == arr2);
+
+let obj1 = { a: 1, b: 2 };
+let obj2 = { a: 1, b: 2 };
+console.log(obj1 == obj2);
+
+let func1 = function () {};
+let func2 = function () {};
+console.log(func1 == func2);
