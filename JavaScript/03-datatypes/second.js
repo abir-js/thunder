76a3035043daf0540dee4c2859b0a17b8dfd24ec
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2026-06-11 15:51:06 +0530

    Add examples of logical operators and hoisting in JavaScript

---
+++ JavaScript/03-datatypes/second.js
@@ -0,0 +1,32 @@
+// hoisting
+// console.log(age);
+// var age = 10;
+
+// console.log(20 + "5"); // 205
+// console.log("20" + 5); // 205
+
+// console.log(null == undefined);
+// console.log(null == 0);
+
+// console.log(null >= 0);
+// console.log(null <= 0);
+// console.log(null > 0);
+// console.log(null < 0);
+
+// console.log(0 === false);
+
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+console.log("hi" || "hello");
+console.log(false || "hello");
+console.log("" || undefined);
+
+console.log(0 || null);
