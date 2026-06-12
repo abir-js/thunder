fd75a5bbf6eec2d54c8decd3d1ef2c92099e6b4e
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2026-06-13 01:58:00 +0530

    Add examples of bitwise operators and number parsing in JavaScript

---
+++ JavaScript/04-operator-and-math-object/first.js
@@ -0,0 +1,6 @@
+// Bitwise Operator
+
+let a = 5;
+let b = 3;
+
+console.log(a & b);
---
+++ JavaScript/04-operator-and-math-object/second.js
@@ -0,0 +1,4 @@
+// parseInt and parseFloat
+console.log(Number("10px"));
+console.log(parseInt("10px"));
+console.log(parseFloat("10px"));
