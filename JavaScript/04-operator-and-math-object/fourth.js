21b4cc9d2cd45d29707c6c10a713f9e3ceb8ef03
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2026-06-14 03:13:53 +0530

    Add examples of Math object methods and number formatting in JavaScript

---
+++ JavaScript/04-operator-and-math-object/fourth.js
@@ -0,0 +1,7 @@
+// Math Object
+
+let a = -23;
+// console.log(Math.round(5.5));
+
+// Math.random()
+console.log(Math.ceil(Math.random() * 10));
---
+++ JavaScript/04-operator-and-math-object/third.js
@@ -0,0 +1,6 @@
+let num = 10.34321245;
+
+console.log(num.toFixed(2));
+console.log(num.toPrecision(2));
+
+console.log(Number("10sd"));
