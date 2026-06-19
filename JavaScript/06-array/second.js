28f3cc34f6b1749275c935ae2de7d0e34d5d1c90
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2026-06-19 13:03:24 +0530

    Add sorting examples for arrays in JavaScript

---
+++ JavaScript/06-array/second.js
@@ -0,0 +1,11 @@
+const names = ["Rohan", "Sohan", "Mohan", "Gohan"];
+
+names.sort();
+console.log(names);
+
+const nums = [10, 20, 7, 101, 23, 78, 4];
+
+nums.sort();
+console.log(nums);
+nums.sort((a, b) => a - b);
+console.log(nums);cd 
