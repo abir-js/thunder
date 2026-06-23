aeb51e2f02e319f230eee6e72d5ae1d9b674e18c
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2026-06-23 14:37:07 +0530

    Add date manipulation examples in JavaScript

---
+++ JavaScript/08-date/first.js
@@ -0,0 +1,18 @@
+// const now = new Date();
+
+// console.log(now);
+// console.log(now.toString());
+
+// console.log(now.getDay());
+// console.log(now.getDate());
+// console.log(now.getMonth());
+// console.log(now.getFullYear());
+
+const now = Date.now();
+// console.log(now);
+
+const da = new Date(-9999999999999);
+// console.log(da);
+
+const myDate = new Date(2025, 5, 21, 6, 30, 10, 0);
+console.log(myDate);
